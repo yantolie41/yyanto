3c79405a3f658f1821f9dd99a4bc840a8f848fa8
yantolie41 <48715287+yantolie41@users.noreply.github.com>
2021-01-14 01:03:39 +1100

    Adding nodeJS file and configuration

---
+++ dev/index.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
