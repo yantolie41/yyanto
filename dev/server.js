082f74500e4862f7d4082ab44ca766c0581a10fd
yantolie41 <48715287+yantolie41@users.noreply.github.com>
2021-01-21 21:57:28 +1100

    Adding views and images

--- dev/server.js
+++ dev/server.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 8000;
 const handlebars =  require('express-handlebars')
+const path = require('path')
 
-app.set('view-engine','hbs')
+app.set('view engine','hbs')
+app.set('views', path.join(__dirname, "views"))
+
+app.engine('hbs', handlebars({
+  layoutsDir:  __dirname + '/views/layouts',
+  extname: 'hbs',
+  defaultLayout: 'main'
+}))
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('main', {layout: 'index'});
+
+  res.render('main', {layout: 'index'})
 });
 
 app.listen(port, () => {
